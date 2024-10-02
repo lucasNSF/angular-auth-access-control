6d426f86cb0be21bbde667baaf28f2b0d0d09426
lucasnsf <nascimentomr10@gmail.com>
2024-10-01 22:00:18 -0300

    feat: installing firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,20 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 
+import { environment } from '../environments/environment';
 import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { DialogService } from 'primeng/dynamicdialog';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp(environment)),
+    provideAuth(() => getAuth()),
+    DialogService,
   ],
 };
